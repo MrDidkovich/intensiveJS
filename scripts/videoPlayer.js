e0583c95b067e36a66220c749ab9df1810cb2ace
Konstantin <ksodar74ru@gmail.com>
2020-07-29 19:38:50 +0500

    add radio player with change radio station

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,10 +1,4 @@
 export const videoPlayerInit = () => {
-	//video-player
-	//video-button__play
-	//video-button__stop
-	//video-time__passed
-	//video-progress
-	//video-time__total
 		const videoPlayer = document.querySelector('.video-player');
 		const videoButtonPlay = document.querySelector('.video-button__play');
 		const videoButtonStop = document.querySelector('.video-button__stop');
